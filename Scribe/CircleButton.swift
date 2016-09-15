cadffc23937b4329e801e66f20f94ee52511c6cf
Ukiand1 <Uros@macbook-pro.local>
2016-09-15 11:07:55 +0200

    Added the button and text to the app, trying to set the corner radius

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Uroš Đurđević on 15/09/16.
+//  Copyright © 2016 Uroš Đurđević. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        didSet{
+           setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView(){
+        layer.cornerRadius = cornerRadius
+    }
+
+}
